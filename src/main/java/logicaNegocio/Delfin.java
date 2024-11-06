25c9a7e0ccba56e9c166af223ecd76b9b07cd9bd
usuario <usuario@gmail.com>
2024-11-06 18:42:07 -0500

    Clases TipoHabitat, Delfin y Animal

--- src/main/java/logicaNegocio/Delfin.java
+++ src/main/java/logicaNegocio/Delfin.java
@@ -1,5 +1,21 @@
 package logicaNegocio;
 
-public class Delfin {
-    // TODO: Aquí va tu código
+public class Delfin extends Animal {
+
+    private TipoHabitat habitat;
+
+    public Delfin() {
+        this.habitat = TipoHabitat.ACUATICO;
+    }
+
+    @Override
+    public String emitirSonido() {
+        return "Chirrido";
+    }
+
+    @Override
+    public String obtenerDieta() {
+        return "Pescado";
+    }
+
 }
