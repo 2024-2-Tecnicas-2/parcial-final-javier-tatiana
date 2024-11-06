648bd911a940ff7e8cb27b6494b91ca1d5d9cf6a
Jhon <Doe@jhon.com>
2024-11-06 18:44:33 -0500

    clase Domestico, Leon, Perro

--- src/main/java/logicaNegocio/Leon.java
+++ src/main/java/logicaNegocio/Leon.java
@@ -1,5 +1,24 @@
 package logicaNegocio;
 
-public class Leon {
-    // TODO: Aquí va tu código
+public class Leon extends Animal {
+
+    private TipoHabitat habitat;
+
+    public Leon() {
+        this.habitat = TipoHabitat.TERRESTRE;
+    }
+
+    @Override
+    public String emitirSonido() {
+        return "Rugido";
+    }
+
+    @Override
+    public String obtenerDieta() {
+        return "Carnívoro";
+    }
+
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
 }
--- src/main/java/logicaNegocio/Perro.java
+++ src/main/java/logicaNegocio/Perro.java
@@ -1,5 +1,27 @@
 package logicaNegocio;
 
-public class Perro {
-    // TODO: Aquí va tu código
+public class Perro extends Animal implements Domestico {
+
+    private TipoHabitat habitat;
+
+    public Perro() {
+        this.habitat = TipoHabitat.TERRESTRE;
+    }
+
+    public String emitirSonido() {
+        return "Ladrido";
+    }
+
+    public String obtenerDieta() {
+        return "Omnívoro";
+    }
+
+    @Override
+    public String interactuarConHumano() {
+        return "El perro mueve la cola y ladra de felicidad";
+    }
+
+    public TipoHabitat getHabitat() {
+        return habitat;
+    }
 }
